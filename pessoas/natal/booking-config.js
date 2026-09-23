f9de0e182f128261dbae7032b6b97ee75089b904
infoforadacasta-max <info.foradacasta@gmail.com>
2026-09-23 21:17:06 +0100

    Ligar reservas de Natal ao calendário CASTA

--- pessoas/natal/booking-config.js
+++ pessoas/natal/booking-config.js
@@ -1,2 +1,2 @@
-// Paste the deployed Google Apps Script /exec URL here once the CASTA account authorizes it.
-window.CASTA_NATAL_BOOKING_URL = '';
+// Google Apps Script de reservas CASTA Natal 2026.
+window.CASTA_NATAL_BOOKING_URL = 'https://script.google.com/macros/s/AKfycbxRHEk5ZLg06l7GJE9EdZDU8y9OiRjOwf-X3Gw7zF0VLAVR6GES2gyA05AuKmk4ItPI/exec';
